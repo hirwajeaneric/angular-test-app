9a05ff0f0fe6f1793d1d3d4f2288ff62e86748c6
HIRWA Jean Eric <hirwaminerve25@gmail.com>
2025-05-12 14:00:21 +0200

    first comit

---
+++ src/app/app.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'My first angular app';
+  yourName: string | null = 'John Doe';
+  greetings = 'Welcome to Angular';
+}
